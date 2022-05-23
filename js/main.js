727c10829b6ed52c6a45ae843bb8526c6640009a
Giaki22 <giaki22@hotmail.com>
2022-05-23 16:14:15 +0200

    + Milestone 1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        toDoList: [
+            {
+                text: 'prova',
+                done: false,
+            },
+            {
+                text: 'prova 2',
+                done: true,
+            }
+        ],
+        index : 0,
+    },
+    methods: {
+        
+    }
+})
